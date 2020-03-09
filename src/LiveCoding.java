c73a637f92aab1f0e60f07dd92ab7fec4fbf1923
Dan DeBlasio <dfdeblasio@utep.edu>
2020-03-08 20:40:39 -0600

    Comments

--- src/LiveCoding.java
+++ src/LiveCoding.java
@@ -1,17 +1,17 @@
 
 public class LiveCoding {
-	
-	/********************************************************************************
-	 * 																				*
-	 * 																				*
-	 *       posted to https://github.com/CS2401-Spring2020/inClass9March.git       *
-	 * 																				*
-	 * 																				*
-	 ********************************************************************************/
+    
+    /********************************************************************************
+     *                                                                              *
+     *                                                                              *
+     *       posted to https://github.com/CS2401-Spring2020/inClass9March.git       *
+     *                                                                              *
+     *                                                                              *
+     ********************************************************************************/
 
-	public static void main(String[] args) {
-		System.out.println(AminoAcidResources.getAminoAcidFromCodon("AAA"));
+    public static void main(String[] args) {
+        System.out.println(AminoAcidResources.getAminoAcidFromCodon("AAA"));
 
-	}
+    }
 
 }
