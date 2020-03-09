161ddd692c9350ac203fcec1388c06e576e8425b
Dan DeBlasio <dfdeblasio@utep.edu>
2020-03-09 09:29:55 -0600

    AA Resources In Class

--- src/LiveCoding.java
+++ src/LiveCoding.java
@@ -10,8 +10,28 @@ public class LiveCoding {
      ********************************************************************************/
 
     public static void main(String[] args) {
-        System.out.println(AminoAcidResources.getAminoAcidFromCodon("AAA"));
-        System.out.println(4%0);  
+        System.out.println(AminoAcidResources.getAminoAcidFromCodon("GUC")); 
+        String[] codonList = AminoAcidResources.getCodonListForAminoAcid('C');
+        for(int i=0; i<codonList.length; i++) {
+        	
+        	if(i == codonList.length-1) {
+        		System.out.println(codonList[i]);
+        	}else {
+        		System.out.print(codonList[i] + ",");
+        	}
+        }
+        
+
+        //given a codon cdn, print all other codons that encode the same aa
+        String cdn = "ACU";
+        String[] derivedList = AminoAcidResources.getCodonListForAminoAcid(AminoAcidResources.getAminoAcidFromCodon(cdn));
+        for(int j=0; j<derivedList.length; j++) {
+        	if(j == derivedList.length-1) {
+        		System.out.println(derivedList[j]);
+        	}else {
+        		System.out.print(derivedList[j] + ",");
+        	}
+        }
     }
 
 }
