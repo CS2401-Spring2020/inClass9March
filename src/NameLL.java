19d9e6cb46a1808fb274d5ac3280216a84e0ca65
cvchavez2 <cvchavez92@gmail.com>
2020-03-12 17:37:33 -0600

    Added note clarifying counter functionality is not fully implemented

--- src/NameLL.java
+++ src/NameLL.java
@@ -1,3 +1,13 @@
+/*
+Carlos Chavez
+CS2401
+Demo done in class Wednesday, March 11 2020
+Note: Recall that adding a counter functionality was not fully implemented
+by the end of the lab. Currently NameLL is adding every name to the
+end of the list, repeated or not, NOT incrementing the counter.
+Please consider that if you are using this class as a reference for your lab
+ */
+
 public class NameLL {
 
     String name;
